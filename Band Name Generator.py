653e720c60edc65957c0d11aa23048864a11cc0b
AbramPeterson <abe.peterson44@gmail.com>
2024-10-13 13:45:10 -0600

    Band Name Generator

---
+++ Band Name Generator.py
@@ -0,0 +1,4 @@
+print("Welcome to Abrams Band Name Generator!")
+city_name = input("What city did you grow up in?\n")
+their_pet_name = input("What was your first pets name?\n")
+print("Your band name could be: " + city_name + " " + their_pet_name)

1e075184a847e5610706a3f051f0272441f5077b
Abram Peterson <119267885+AbramPeterson@users.noreply.github.com>
2024-10-13 13:51:10 -0600

    Delete Band Name Generator.py

--- Band Name Generator.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to Abrams Band Name Generator!")
-city_name = input("What city did you grow up in?\n")
-their_pet_name = input("What was your first pets name?\n")
-print("Your band name could be: " + city_name + " " + their_pet_name)
